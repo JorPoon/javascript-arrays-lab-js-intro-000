4b1d8d266d46680ef79ecad91530b3b96ad3bcaa
Jor Ming Poon <jormpoon@gmail.com>
2018-10-01 17:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions and code here
+var destructivelyAppendKitten = (name) => {
+  kittens.push(name);
+}
+
+var destructivelyPrependKitten = ( name ) => {
+  kittens.unshift(name);
+}
+
+var destructivelyRemoveLastKitten = () => {
+  kittens.pop();
+}
+
+var destructivelyRemoveFirstKitten =  () => {
+  kittens.shift();
+}
+
+var appendKitten = ( name ) => {
+  return [...kittens, name]
+
+}
